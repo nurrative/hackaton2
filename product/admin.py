c762c8938ae107ce2b964d5bcf868472c8ad78a5
nurrative <nursul1997@gmail.com>
2023-05-01 23:30:19 +0600

    /

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Product, Subcategory, Category
 
-admin.site.register(Subcategory)
+
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Subcategory)
