da657cb5df710433baa696ab11d5bbbffabba23c
nurrative <nursul1997@gmail.com>
2023-05-04 17:53:22 +0600

    delete test database

--- config/settings.py
+++ config/settings.py
@@ -106,14 +106,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },
-    'test': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('TEST_DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+   
 }
 
 
