c038e5b01bc6404e96e809a8341a7927e0199444
nurrative <nursul1997@gmail.com>
2023-05-12 15:21:12 +0600

    settings

--- config/settings.py
+++ config/settings.py
@@ -165,7 +165,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 #     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 # )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
